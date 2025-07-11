0ca9ec589b94cc066ac17eb8fc7c343e9fb4bcf1
rain9 <15911122312@163.com>
2025-07-11 18:14:59 +0800

    chore: update name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.joker.flutter_tdkit"
+    namespace = "com.rain.flutter_tdkit"
     compileSdk = flutter.compileSdkVersion
 //    ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.joker.flutter_tdkit"
+        applicationId = "com.rain.flutter_tdkit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
